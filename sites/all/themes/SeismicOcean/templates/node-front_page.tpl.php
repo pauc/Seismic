ff4cc2735dcc3a94ba10cef73b43052ee9d376a2
Pau Compte <pausandalio@gmail.com>
2011-12-23 10:27:35 +0100

    Afegida background-image als enllaços node_read_more. Vies slideshow només a <front> afegint if ($teaser) a node-front_page.tpl.php

--- sites/all/themes/SeismicOcean/templates/node-front_page.tpl.php
+++ sites/all/themes/SeismicOcean/templates/node-front_page.tpl.php
@@ -97,7 +97,9 @@
   <?php endif; ?>
 
   <div class="content">
-    <?php print views_embed_view('front_page_slideshow'); ?>
+    <?php if($teaser): ?>
+      <?php print views_embed_view('front_page_slideshow'); ?>
+    <?php endif; ?>
     <?php if($is_front): ?>
         <h2 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     <?php endif; ?>
