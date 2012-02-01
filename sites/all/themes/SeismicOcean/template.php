a56ab109b2ba0eda062c5afc3ef928deafec1dbd
Pau Compte <pausandalio@gmail.com>
2012-02-02 00:39:07 +0100

    Afegida funció a template.php per a que el fieldset de nodehierarchy no surti col·lapsat

--- sites/all/themes/SeismicOcean/template.php
+++ sites/all/themes/SeismicOcean/template.php
@@ -447,3 +447,33 @@ function SeismicOcean_print_format_link() {
                'attributes' => print_fill_attributes($title, $class, $new_window),
               );
 }
+
+/**
+ * Format a group of form items.
+ *
+ * @param $element
+ *   An associative array containing the properties of the element.
+ *   Properties used: attributes, title, value, description, children, collapsible, collapsed
+ * @return
+ *   A themed HTML string representing the form item group.
+ *
+ * @ingroup themeable
+ */
+function SeismicOcean_fieldset($element) {
+  if (!empty($element['#collapsible'])) {
+    drupal_add_js('misc/collapse.js');
+
+    if (!isset($element['#attributes']['class'])) {
+      $element['#attributes']['class'] = '';
+    }
+
+    $element['#attributes']['class'] .= ' collapsible';
+    if (!empty($element['#collapsed'])) {
+      if ($element['#title'] != 'Node Hierarchy') {
+        $element['#attributes']['class'] .= ' collapsed';
+      }
+    }
+  }
+
+  return '<fieldset'. drupal_attributes($element['#attributes']) .'>'. ($element['#title'] ? '<legend>'. $element['#title'] .'</legend>' : '') . (isset($element['#description']) && $element['#description'] ? '<div class="description">'. $element['#description'] .'</div>' : '') . (!empty($element['#children']) ? $element['#children'] : '') . (isset($element['#value']) ? $element['#value'] : '') ."</fieldset>\n";
+}
