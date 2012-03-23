4b09848e30d91e34045ec600d688dd1760c7c619
Pau Compte <pausandalio@gmail.com>
2012-03-23 06:32:26 +0100

    Actualitzat nucli i mòdul CKEditor

---
+++ sites/all/themes/SeismicOcean/js/add_target_to_biblio_links.js
@@ -0,0 +1,4 @@
+$(function(){
+  $('body.node-type-biblio td.biblio-field-contents-url a').attr('target', '_blank');
+  $('body.node-type-biblio td.biblio-field-contents-doi a').attr('target', '_blank');
+});
