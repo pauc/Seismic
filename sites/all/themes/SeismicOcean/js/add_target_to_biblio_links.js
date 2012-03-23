0104121b4576570768d962801fcffebc9e5b3d7d
Pau Compte <pausandalio@gmail.com>
2012-03-23 06:45:39 +0100

    minuscul

--- sites/all/themes/SeismicOcean/js/add_target_to_biblio_links.js
+++ sites/all/themes/SeismicOcean/js/add_target_to_biblio_links.js
@@ -1,4 +1,6 @@
 $(function(){
-  $('body.node-type-biblio td.biblio-field-contents-url a').attr('target', '_blank');
-  $('body.node-type-biblio td.biblio-field-contents-doi a').attr('target', '_blank');
+  if ($('body').hasClass('node-type-biblio')){
+    $('td.biblio-field-contents-url a').attr('target', '_blank');
+    $('td.biblio-field-contents-doi a').attr('target', '_blank');    
+  }
 });
